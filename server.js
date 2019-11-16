e1692a07d9a00ba44b87cfe6444a72104d241580
kal-J <kharljay15@gmail.com>
2019-11-16 15:56:57 +0300

    comment out the the home route

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const app = express();
 app.use(cors());
 app.use(express.static("src"));
 
+/*
 app.get('/', (req,res) => {
     res.sendFile('/index.html', err => console.log(err))
 });
+*/
 
 app.listen(process.env.PORT, () =>
   console.log(`App running on port ${process.env.PORT}`)
